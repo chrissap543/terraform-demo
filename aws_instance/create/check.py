5520eb0c5c3b01bf5ae7a15f9b03f87def95f1d7
chrissap543 <cs79@illinois.edu>
2024-09-23 13:50:26 +0200

    added check.py

---
+++ aws_instance/create/check.py
@@ -0,0 +1,11 @@
+import json
+import sys
+
+with open('plan.json', 'r') as file:
+    data = json.load(file)
+
+for i in data['planned_values']['root_module']['resources']:
+    if i['values']['tags'] is None or "Name" not in i['values']['tags']:
+        sys.exit(1)
+    else:
+        print("found name tag")
